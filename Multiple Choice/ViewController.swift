337d8fe7d31cb9fcfdf664214531114a9321b9b4
Landon Follows <lfollows@lcs.on.ca>
2019-12-09 15:04:55 -0500

    Created Input, Corrected Code, Testing it out until final.

--- Multiple Choice/ViewController.swift
+++ Multiple Choice/ViewController.swift
@@ -9,12 +9,85 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+        // MARK: Labels
+    
+    @IBOutlet weak var Questions: UITextField!
+    
+    @IBOutlet weak var Student: UITextField!
+    
+    @IBOutlet weak var theAnswers: UITextField!
+    
+    @IBOutlet weak var result: UITextView!
+    
+        // MARK: Methods
+    
+    @IBAction func check(_ sender: Any) {
+        
+        // MARK: Guard
+        
+        guard let QuestionsAsString = Questions.text, QuestionsAsString != "" else {
+            
+            result.text = "please enter an integer value greater than 0 "
+            
+            return
+        }
+        guard let Student = Student.text, Student != "" else {
+            
+            result.text = "Make Sure you input the exact \(QuestionsAsString) student answers"
+            
+            return
+        }
+        
+        guard let theAnswers = theAnswers.text, theAnswers != "" else {
+            
+            result.text = "Make Sure you input the exact \(QuestionsAsString) answers for the answer key"
+            
+            return
+            
+        }
+        
+        var offset = 0
+        
+        //make variable for charcters
+        
+        var score = 0
+        
+        //Find out the score value
+        
+        for _ in theAnswers {
+            
+            //get the answers from the student answers
+            
+            let indexStudent = Student.index(Student.startIndex, offsetBy: offset)
+            
+            let Student = Student[indexStudent]
+            
+            //Get Correct answers
+            
+            let indexAnswer = theAnswers.index(theAnswers.startIndex, offsetBy: offset)
+            
+            let theAnswers = theAnswers[indexAnswer]
+            
+            if Student == theAnswers {
+                
+                score += 1
+                
+            }
+            
+            offset += 1
+            
+            result.text = "The Student Answered \(score) question(s) correctly!"
+            
+        }
+    }
 }
 
